e5ff4c4d6164dfe376d562cfeff5413b7ae9a9f5
Suvorov Luka <suvorovluka@gmail.com>
2023-06-14 21:41:51 +0300

    add Redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useSelector,useDispatch} from 'react-redux';
+
+import { setCategoryId } from '../redux/slices/filterSlice';
 
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
@@ -9,24 +12,32 @@ import { AppContext } from '../App';
 
 
 const Home = () => {
-    const {searchValue}=React.useContext(AppContext)
+    const categoryId = useSelector((state) => state.filter.categoryId);
+    const dispatch = useDispatch()
+
+    const { searchValue } = React.useContext(AppContext)
     const [items, setItems] = React.useState([]);
     const [loading, setLoading] = React.useState(true);
-    const [categoryID, setCategoryID] = React.useState(0);
+    // const [categoryId, setCategoryId] = React.useState(0);
     const [currentPage, setCurrentPage] = React.useState(1);
     const [sortType, setSortType] = React.useState({
         name: 'популярности',
         sortProperty: 'rating'
     });
 
+    const onClickCategory=(id)=>{
+        dispatch(setCategoryId(id));
+    }
+
+
     React.useEffect(() => {
         setLoading(true)
 
         const sortBy = sortType.sortProperty;
-        const category = categoryID > 0 ? `category=${categoryID}` : '';
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
         const search = searchValue ? `&search=${searchValue}` : '';
 
-        fetch(`https://64831bcef2e76ae1b95bf40f.mockapi.io/items?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=desc${search}`
+        fetch(`https://64831bcef2e76ae1b95bf40f.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=desc${search}`
         )
             .then((res) => res.json())
             .then((arr) => {
@@ -34,7 +45,7 @@ const Home = () => {
                 setLoading(false)
             });
         window.scroll(0, 0)
-    }, [categoryID, sortType, searchValue,currentPage]);
+    }, [categoryId, sortType, searchValue, currentPage]);
 
     const pizzas = items.map((obj) => <Pizzablock key={obj.id} {...obj} />)
     const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />)
@@ -42,7 +53,7 @@ const Home = () => {
     return (
         <div className="container">
             <div className="content__top">
-                <Categories value={categoryID} onClickCategory={(id) => setCategoryID(id)} />
+                <Categories value={categoryId} onClickCategory={onClickCategory} />
 
                 <Sort
                     value={sortType} onChangeSort={(property) => setSortType(property)}
